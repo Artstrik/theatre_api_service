0a6139615121462398bf37a962440e654f4999c7
Artem Shlychkin <artemshlychkin1995@gmail.com>
2025-12-19 09:42:52 +0200

    configure endpoints for theatre API

---
+++ theatre/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from theatre.views import (
+    GenreViewSet,
+    ActorViewSet,
+    TheatreHallViewSet,
+    PlayViewSet,
+    PerformanceViewSet, ReservationViewSet,
+
+)
+
+app_name = "theatre"
+
+router = routers.DefaultRouter()
+
+router.register("genres", GenreViewSet)
+router.register("actors", ActorViewSet)
+router.register("plays", PlayViewSet)
+router.register("theatre_hall", TheatreHallViewSet)
+router.register("performances", PerformanceViewSet)
+router.register("reservations", ReservationViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
